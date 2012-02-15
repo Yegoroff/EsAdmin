33d4ac6e34e968de20dbce82fd6551951ff449d4
Yegoroff <AlexYegoroff+github@gmail.com>
2012-02-15 19:01:20 +0200

    Merge

--- EsAdmin/ElasticSearch/EsConnector.cs
+++ EsAdmin/ElasticSearch/EsConnector.cs
@@ -17,7 +17,8 @@ namespace EsAdmin
         {
             textToExecute = textToExecute.Trim(' ', '\r', '\n', '\t');
 
-            var splittedText = textToExecute.Trim(' ', '\r', '\n').Split(new[] { " ", "\r\n", "\t" }, StringSplitOptions.RemoveEmptyEntries);
+            var splittedText = textToExecute.Trim(' ', '\r', '\n').Split(new[] {" ", "\r\n", "\t"},
+                                                                         StringSplitOptions.RemoveEmptyEntries);
             string action = splittedText[0];
             string path = splittedText[1];
             path = UrlBuilder.AddParameter(path, "pretty", "true");
@@ -25,29 +26,30 @@ namespace EsAdmin
             int endOfFirstLine = textToExecute.IndexOf("\r\n");
             string body = "";
             if (endOfFirstLine > 0)
-                body = textToExecute.Remove(0, endOfFirstLine + 2);            
+                body = textToExecute.Remove(0, endOfFirstLine + 2);
 
-            switch (action.ToUpper())
-            {
-                case "GET":
-                    if (string.IsNullOrWhiteSpace(body))
+                switch (action.ToUpper())
+                {
+                    case "GET":
+                        if (string.IsNullOrWhiteSpace(body))
                         return Connection.Get(path);
                     return Connection.Post(path, body);
 
-                case "POST":
+                    case "POST":
                     return Connection.Post(path, body);
 
-                case "PUT":
+                    case "PUT":
                     return Connection.Put(path, body);
 
-                case "DELETE":
+                    case "DELETE":
                     return Connection.Delete(path, body);
 
-                default:
+                    default:
                     throw new ArgumentException("Invalid Action {0} detected".F(action),"textToExecute");
+                }
+
+            {
             }
-
-
         }
     }
 }
